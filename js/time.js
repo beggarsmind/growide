d378299f5122ff4353cf766fc0ee0d4d31c43353
Ranjithkumar R <155801465+beggarsmind@users.noreply.github.com>
2025-05-06 16:37:30 +0530

    Update time.js

--- js/time.js
+++ js/time.js
@@ -1,18 +1,24 @@
 function Clock() {
   const now = new Date();
 
+  const dayOptions = { weekday: 'long' };
+  const dateOptions = { year: 'numeric', month: 'long', day: 'numeric' };
+
+  const day = now.toLocaleDateString(undefined, dayOptions);
+  const date = now.toLocaleDateString(undefined, dateOptions);
+
   const timeOptions = {
     hour: '2-digit',
     minute: '2-digit',
     second: '2-digit',
     hour12: true,
   };
+  let time = now.toLocaleTimeString(undefined, timeOptions);
+  time = time.replace(/am|pm/i, (match) => match.toUpperCase());
 
-  const time = now
-    .toLocaleTimeString(undefined, timeOptions)
-    .replace(/am|pm/i, (match) => match.toUpperCase());
-
-  document.getElementById('time').textContent = 'Time: ' + time;
+  document.getElementById('day').innerHTML = `Day: ${day}`;
+  document.getElementById('date').innerHTML = `Date: ${date}`;
+  document.getElementById('clock').innerHTML = `Time: ${time}`;
 }
 
 setInterval(Clock, 1000);
